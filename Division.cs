41db9a57d2e34a1fccf96272191517c0dd5f2b44
jumana727 <jumanajambu23@gmail.com>
2022-02-01 15:55:16 +0530

    commiting

---
+++ Division.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Division
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter numerator:");
+            int numerator = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter denominator:");
+            int denominator = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"The result of integer division is {numerator/denominator} and remainder is {numerator%denominator}");
+            Console.WriteLine($"The result of Floating point division is {(double)numerator / denominator}");
+            Console.WriteLine($"The result of Mixed Fraction division is {numerator / denominator} {numerator % denominator}/{denominator}");
+        }
+    }
+}
